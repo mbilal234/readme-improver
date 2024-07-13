cb3b55b4c13b9b77b6f9bb975f5ab98e4a42360a
Muhammad Bilal <mbilal.bese21seecs@seecs.edu.pk>
2024-07-13 14:11:25 -0400

    Initial logic

---
+++ main.js
@@ -0,0 +1,16 @@
+
+(function ($) {
+    "use strict";
+
+    /*==================================================================
+    [ Validate after type ]*/
+    $('.validate-input .input100').each(function(){
+        $(this).on('blur', function(){
+            if(validate(this) == false){
+                showValidate(this);
+            }
+            else {
+                $(this).parent().addClass('true-validate');
+            }
+        })
+    })
